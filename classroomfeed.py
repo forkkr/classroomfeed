50b627df303f2ed10f6a677bcd9a1161773bbfc9
forkkr <kashobroy@gmail.com>
2018-10-24 18:19:27 +0600

    give a look.

--- classroomfeed.py
+++ classroomfeed.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, redirect, session
-
+from database.database import db
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    #data = {'string1': 'string32', 'string2': 'string22'}
+    #posts = db.hello_world123
+    #post_id = posts.insert_one(data).inserted_id
     return render_template(
         'feed.html', **locals()
     )
