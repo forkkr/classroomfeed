eb313099184a6122de3002838267704f01e077b2
forkkr <kashobroy@gmail.com>
2018-10-24 22:03:47 +0600

    give a look too.

--- classroomfeed.py
+++ classroomfeed.py
@@ -9,8 +9,26 @@ def hello_world():
     #posts = db.hello_world123
     #post_id = posts.insert_one(data).inserted_id
     return render_template(
-        'feed.html', **locals()
-    )
+        'feed.html', **locals() )
+
+
+@app.route("/news-feed")
+def show_news_feed():
+    pymongo_cursor = db.hello_world123.find()
+    all_data = list(pymongo_cursor)
+    data = all_data
+    print(data)
+    return render_template('feed.html', **locals())
+
+
+@app.route("/entry_data" , methods=['POST', 'GET'])
+def update_post():
+    if request.method=="POST":
+        data = request.form
+        data = {"posttext": data['message'],"postfile": data['files']}
+        posts = db.hello_world123
+        #post_id = posts.insert_one(data).inserted_id
+    return redirect('/news-feed')
 
 
 if __name__ == '__main__':
